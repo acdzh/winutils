b475e62eca3b2542178cb45d355ce0278ed93567
acdzh <1069436872@qq.com>
2020-02-13 16:39:11 +0800

    add error catch

--- ip.ps1
+++ ip.ps1
@@ -1,9 +1,9 @@
 ﻿Get-NetIPAddress | Where-Object {('WLAN', '以太网', 'Ethernet') -contains $_.InterfaceAlias} | Format-Table InterfaceAlias, IPAddress
-$ipapi = (Invoke-WebRequest -Uri https://ipapi.co/ip).Content
+$ipapi = try {(Invoke-WebRequest -Uri https://ipapi.co/ip).Content} catch {'Net Error'}
 Write-Host 'ipapi         ' $ipapi
-$ip_api_ip = (ConvertFrom-Json (Invoke-WebRequest http://ip-api.com/json)).query
+$ip_api_ip = try {(ConvertFrom-Json (Invoke-WebRequest http://ip-api.com/json)).query} catch {'Net Error'}
 Write-Host 'ip-api ip     ' $ip_api_ip
-$ip_api_dns = (ConvertFrom-Json (Invoke-WebRequest http://edns.ip-api.com/json)).dns.ip
+$ip_api_dns = try{(ConvertFrom-Json (Invoke-WebRequest http://edns.ip-api.com/json)).dns.ip} catch {'Net Error'}
 Write-Host 'ip-api dns    ' $ip_api_dns
-$ipgoji = (Invoke-WebRequest -Uri http://ip-goji.appspot.com/text).Content
+$ipgoji = try{(Invoke-WebRequest -Uri http://ip-goji.appspot.com/text).Content} catch{'Net Error'}
 Write-Host 'ip-goji       ' $ipgoji
